6ece7bc5bb3e4c99affd16bfc88e138ddfeebd3d
Catomrad <91577742+Catomrad@users.noreply.github.com>
2024-12-14 21:47:49 +0700

    add footer, modal and connect Formspree

---
+++ main/static/js/modal.js
@@ -0,0 +1,22 @@
+const modal = document.getElementById('modal');
+const openModalBtns = document.getElementsByClassName('openModalBtn'); // Используем множественное число для переменной
+const closeModalBtn = document.getElementById('closeModalBtn');
+
+// Открываем модальное окно
+Array.from(openModalBtns).forEach(btn => { // Преобразуем коллекцию в массив и перебираем
+    btn.addEventListener('click', () => {
+        modal.style.display = 'flex'; // Убедитесь, что display установлен в flex
+    });
+});
+
+// Закрываем модальное окно
+closeModalBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Закрываем модальное окно при клике вне его
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
